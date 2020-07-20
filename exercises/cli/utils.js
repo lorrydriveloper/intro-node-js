4a21955edd01f07ddbc9c98482b5395d921203d3
lorrydriveloper <lorrydriveloper@gmail.com>
2020-07-20 14:27:27 +0100

    Fix small nodejs CLI exercise.

--- exercises/cli/utils.js
+++ exercises/cli/utils.js
@@ -1,17 +1,18 @@
-const fs = require('fs')
-const path = require('path')
+const fs = require("fs");
+const path = require("path");
 
 // this path needs to be relative to work with fs
-const contactsLocation = 'contacts.json'
-
+const contactsLocation = path.join(__dirname, "contacts.json");
 /**
  * should read the contacts at the
  * @contactsLocation path and convert
  * it to a js object
  */
 const getContacts = () => {
-  
-}
+  // console.log(contactsLocation);
+  const file = fs.readFileSync(contactsLocation).toString();
+  return JSON.parse(file);
+};
 
 /**
  * takes a contacts object, converts it to JSON
@@ -19,12 +20,12 @@ const getContacts = () => {
  * @param {Object} contacts contacts object
  */
 const saveContacts = (contacts) => {
-
-}
+  const data = JSON.stringify(contacts);
+  fs.writeFileSync(contactsLocation, data);
+};
 
 module.exports = {
   contactsLocation,
   getContacts,
-  saveContacts
-}
-
+  saveContacts,
+};
